fcade05bec75bc6be7d514e0e5d7f17c70c89e27
Peter Njoroge <njugunahpeternjoroge@gmail.com>
2026-09-09 00:19:41 +0300

    Add project files.

---
+++ JwtWebApiTutorial/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpPost("register")]
+        [ApiController]
+        public class AuthController: ControllerBase
+        {
+
+        }
+    }
+}
---
+++ JwtWebApiTutorial/User.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtWebApiTutorial
+{
+    public class User
+    {
+
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public byte[] PasswordSalt { get; set; }
+    }
+}
